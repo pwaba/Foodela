fc6aeb88b24b9ebac23ecfda493044f3b5ae50e6
Peter <peter.walleback@gmail.com>
2016-11-28 20:47:42 +0100

    Scrollista

---
+++ app/src/main/java/com/example/android/myapplication/users/UserHandler.java
@@ -0,0 +1,49 @@
+package com.example.android.myapplication.users;
+
+
+import android.content.Context;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * UserHandler keeps user information and provides an interface where that information can be
+ * achieved and methods for checking if a user exists among the allowed ones.
+ * Created by peter on 2016-11-03.
+ */
+
+public class UserHandler {
+
+    private List<User> users;
+
+    public UserHandler(Context context) {
+
+        this.users = new ArrayList<>();
+
+        this.users.add(new User("Nisse", "nisse@mail.com", "pass1", "nizz"));
+        this.users.add(new User("Tord", "tord@mail.com", "pass2", "MrT"));
+        this.users.add(new User("Lisa", "Lisa@mail.com", "pass3", "Lisa"));
+        this.users.add(new User("Anna", "Anna@mail.com", "pass4", "Anna"));
+
+        WriteXMLFile xmlWriter = new WriteXMLFile(context);
+        xmlWriter.main(null);
+    }
+
+    public List<User> getUsers() {
+        return this.users;
+    }
+
+    /**
+     * @param name
+     * @param password
+     * @return True if the user exists and that the given password is correct
+     */
+    public boolean checkUser(final String name, final String password) {
+
+        for (User user : this.users) {
+            if (user.getName().equals(name) && user.getPassword().equals(password))
+                return true;
+        }
+        return false;
+    }
+}
